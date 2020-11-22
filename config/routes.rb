2b6f6dcb3bdfe9138244da4b28a2d436e4164f9d
Ashton <ashtonschoffner@gmail.com>
2020-11-21 21:48:24 -0600

    Added show view for subcategory

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :brewers, only: %i[index show]
   resources :beers, only: %i[index show]
+  resources :categories, only: %i[index show]
+  resources :subcategories, only: %i[show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
