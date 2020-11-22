7f6316f4aad0ed1a27906d689737c79e907fa779
Ashton <ashtonschoffner@gmail.com>
2020-11-21 21:34:49 -0600

    Added category index and show Added link to category show through beer show

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,13 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.includes(:subcategories).order("name ASC")
+  end
+
+  def show
+    @category = Category.includes(:subcategories).find(params[:id])
+    @beers = Beer.includes(:subcategory)
+                 .where(subcategory_id: @category.subcategories.ids)
+                 .order("name ASC")
+                 .page(params[:page]).per(30)
+  end
+end
