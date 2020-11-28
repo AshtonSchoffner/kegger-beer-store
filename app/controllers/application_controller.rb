94d9e3e6cdbb13907e8fdfac01c7c38684aa0900
Ashton <ashtonschoffner@gmail.com>
2020-11-28 14:04:54 -0600

    Added address and province to the user registration Added user to active_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    added_attrs = %i[username email password password_confirmation remember_me]
+    added_attrs = %i[username email password password_confirmation remember_me address province_id]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -12,7 +12,7 @@ class AdminUser < ApplicationRecord
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    if login == conditions.delete(:login)
+    if login = conditions.delete(:login)
       where(conditions.to_h).where(["lower(email) = :value", { value: login.downcase }]).first
     elsif conditions.key?(:email)
       where(conditions.to_h).first
